80e377151aec0729525d273f7a3519bbf6c0898f
martins-wasami <wasamimartin@gmail.com>
2026-09-23 14:29:12 +0100

    added footer widgets

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/widgets/Footer'
 import styles from './HomePage.module.scss'
 
 import { Header } from '@/widgets/Header'
@@ -7,9 +8,13 @@ const HomePage = () => {
 
 
   return (
-    <>
+    <div className={styles.pageWrapper}>
     <Header/>
-    </>
+    <main className={styles.content}>
+     <h1>home page</h1>
+    </main>
+    <Footer/>
+    </div>
   )
 }
 export default HomePage
